dd6317bd09d8dc78947f7a21fdf88835e1e34618
sweing <sjweingaertner@gmail.com>
2023-05-07 14:46:06 +0200

    adding pca scatterplot

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import json
 import pandas as pd
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
+import random
 
 app = Flask(__name__)
 
@@ -24,6 +25,7 @@ def data():
     plot_df = df[cols_to_keep]
 
     selected_countries = ['Germany', 'France', 'Italy']  # Replace with your list of 40 countries
+    selected_countries = random.sample(list(plot_df["Country Name"]), 40)
     plot_df = plot_df[plot_df['Country Name'].isin(selected_countries)]
     
     grouped_df = plot_df.groupby(['Country Name', 'year']).sum().reset_index()
@@ -32,7 +34,7 @@ def data():
     grouped_df_for_pca = grouped_df[grouped_df['year'] == most_recent_year]
 
     # Standardize for PCA
-    X = grouped_df[plot_variables]
+    X = grouped_df_for_pca[plot_variables]
     scaler = StandardScaler()
     X_std = scaler.fit_transform(X)
 
@@ -43,12 +45,12 @@ def data():
 
     pca_df = pd.DataFrame(pc, columns=['PC1', 'PC2'])
     pca_df.index = X.index
-    pca_df['Country Name'] = grouped_df['Country Name']
+    pca_df['Country Name'] = grouped_df_for_pca['Country Name']
     print(pca_df)
     # Convert to JSON
     data = pca_df.to_json(orient='records')
 
-    return render_template("index.html", pca_df=json.dumps(data), data=json.dumps(df.to_json(orient='records')), variables=plot_variables, selected_countries=selected_countries)
+    return render_template("index.html", pca_df=json.loads(data), data=json.dumps(df.to_json(orient='records')), variables=plot_variables, selected_countries=selected_countries)
 
 if __name__ == '__main__':
     app.run()
---
+++ static/js/scatterplot.js
@@ -0,0 +1,59 @@
+function scatterplot(data) {
+  console.log(data)
+  // Set the dimensions of the plot
+  var margin = {top: 20, right: 20, bottom: 30, left: 50},
+      width = 600 - margin.left - margin.right,
+      height = 300 - margin.top - margin.bottom;
+
+  // Get the max and min values for PC1 and PC2
+  var xValues = data.map(function(d) { return d.PC1; });
+  var yValues = data.map(function(d) { return d.PC2; });
+  var countryValue = data.map(function(d) { return d["Country Name"]; });
+  var xMin = d3.min(xValues);
+  var xMax = d3.max(xValues);
+  var yMin = d3.min(yValues);
+  var yMax = d3.max(yValues);
+
+  // Define the scales for the x and y axis
+  var x = d3.scaleLinear()
+      .domain([xMin, xMax])
+      .range([0, width]);
+
+  var y = d3.scaleLinear()
+      .domain([yMin, yMax])
+      .range([height, 0]);
+
+  // Create the svg element and add it to the scatterplot div
+  var svg = d3.select("#scatterplot").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+
+  // Add the x and y axis to the svg element
+  svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      //.call(d3.axisBottom(x));
+
+  // Add the data points to the svg element
+  svg.selectAll("circle")
+      .data(data)
+    .enter().append("circle")
+      .attr("cx", function(d) { return x(d.PC1); })
+      .attr("cy", function(d) { return y(d.PC2); })
+      .attr("r", 5)
+      .on("mouseover", function(d) {
+        // Get the current circle element
+        var currentCircle = d3.select(this);
+        var currentCountry = currentCircle.data()[0]["Country Name"];
+        console.log(currentCountry)
+        // Update the style of the current circle
+        currentCircle.style("stroke", "red")
+                    .style("stroke-width", "4")
+                    .style("fill-opacity", "0.8");                
+      })
+      .on("mouseout", function(d) {
+        d3.select(this).style("stroke", "none");
+      });
+}
