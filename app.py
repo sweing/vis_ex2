55e9d8556afa7310fc1b3fe8e6e62ead617bf6bd
sweing <sjweingaertner@gmail.com>
2023-05-08 12:16:52 +0200

    lineplot is updated with drop down | todo: select country for lineplot on map click, variable selection for drop down, fix data (fill?) etc.

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 # from sklearn.preprocessing import PCA
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
-import random
+# import random
 
 app = Flask(__name__)
 
@@ -15,18 +15,30 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 # Load data
 df = pd.read_csv('static/data/agriRuralDevelopment_reduced.csv', sep=",")
 
+
+# Get the list of all column names
+all_columns = df.columns.tolist()
+
+# Filter out the columns you don't want based on their names
+non_info_columns = ['Country Name', 'year', 'Country Code', 'Unnamed: 0']
+plot_variables = [col for col in all_columns if col not in non_info_columns]
+
+
 # Define plot variables
-plot_variables = ['Employment in agriculture (% of total employment) (modeled ILO estimate)',
-                  'Agriculture, forestry, and fishing, value added (% of GDP)']
+# plot_variables = ['Employment in agriculture (% of total employment) (modeled ILO estimate)',
+#                   'Agriculture, forestry, and fishing, value added (% of GDP)',
+#                   'Birth rate, crude (per 1,000 people)']
+
+
 
 @app.route('/')
 def data():
     # Select the columns to plot
     # cols_to_keep = plot_variables + ['year', 'Country Name']
     plot_df = df
+    # selected_countries = random.sample(list(plot_df["Country Name"]), 40)
+    selected_countries = ["Australia", "Azerbaijan", "Belarus", "Belgium", "Belize", "Benin", "Bolivia", "Botswana", "Brazil", "Bulgaria", "Burkina Faso", "Burundi", "Cameroon", "Chad", "Colombia", "Croatia", "Denmark", "Dominican Republic", "Ecuador", "Egypt", "Finland", "Gambia", "Georgia", "Ghana", "Guinea", "Haiti", "Honduras", "Indonesia", "Iraq", "Ireland", "Israel", "Jordan", "Kuwait", "Latvia", "Lebanon", "Liberia", "Libya", "Mali", "Mauritania", "Moldova", "Mongolia", "Morocco"]
 
-    selected_countries = ['Germany', 'France', 'Italy']  # Replace with your list of 40 countries
-    selected_countries = random.sample(list(plot_df["Country Name"]), 40)
     plot_df = plot_df[plot_df['Country Name'].isin(selected_countries)]
     
     grouped_df = plot_df.groupby(['Country Name', 'year']).sum().reset_index()
@@ -34,7 +46,7 @@ def data():
     # for line plots
     sel_dat_line = ['Country Name', 'year', plot_variables[0], plot_variables[1]]
     data_line = grouped_df[sel_dat_line]
-    #data_line = data_line.rename(columns={plot_variables[0]: 'employed in agriculture', plot_variables[1]: 'agriculture in GDP'})
+    # data_line = data_line.rename(columns={plot_variables[0]: 'employed in agriculture', plot_variables[1]: 'agriculture in GDP'})
 
     most_recent_year = grouped_df['year'].max()
     grouped_df_for_pca = grouped_df[grouped_df['year'] == most_recent_year]
@@ -52,7 +64,6 @@ def data():
     pca_df = pd.DataFrame(pc, columns=['PC1', 'PC2'])
     pca_df.index = X.index
     pca_df['Country Name'] = grouped_df_for_pca['Country Name']
-    print(pca_df)
     # Convert to JSON
     data = pca_df.to_json(orient='records')
 
--- static/js/lineplot.js
+++ static/js/lineplot.js
@@ -1,55 +1,81 @@
 function lineplot(data) {
-    data = JSON.parse(data)
+  data = JSON.parse(data);
 
-    const filteredData = data.filter(function(d) {
-      return d["Country Name"] === countryForLinePlot;
-    });
+  const filteredData = data.filter(function(d) {
+    return d["Country Name"] === "Belgium";
+  });
 
-    console.log(filteredData)
-      
-    var countryValue = filteredData.map(function(d) { return d["Country Name"]; });
-    var xValues = filteredData.map(function(d) { return d["year"]; });
-    var yValues = filteredData.map(obj => obj["Birth rate, crude (per 1,000 people)"]);
-    console.log(yValues)
-    // Define the dimensions and margins of the chart area
-    var margin = {top: 20, right: 20, bottom: 30, left: 50},
-        width = 960 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;
+  var countryValue = filteredData.map(function(d) { return d["Country Name"]; });
+  var xValues = filteredData.map(function(d) { return d["year"]; });
+  var yValues = getYValues(filteredData, selectedOption.value);
 
-    // Create an SVG element and append it to the DOM
-    var svg = d3.select("body").append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  var margin = {top: 20, right: 20, bottom: 30, left: 50},
+      width = 960 - margin.left - margin.right,
+      height = 500 - margin.top - margin.bottom;
 
-    // Define the scales for the x and y axes
-    var x = d3.scaleLinear()
-        .domain(d3.extent(xValues))
-        .range([0, width]);
+  var svg = d3.select("body").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    var y = d3.scaleLinear()
-        .domain(d3.extent(yValues))
-        .range([height, 0]);
+  var x = d3.scaleLinear()
+      .domain(d3.extent(xValues))
+      .range([0, width])
 
-      // Define the line generator function to map data to coordinates
-    var line = d3.line()
-      .x(function(d, i) { return x(xValues[i]); })
-      .y(function(d, i) { return y(yValues[i]); });
+  var y = d3.scaleLinear()
+      .domain(d3.extent(yValues))
+      .range([height, 0]);
 
-      // Append the line to the SVG element
-     svg.append("path")
-        .datum(filteredData)
-        .attr("class", "line")
-        .attr("d", line)
-        .style("fill", "none")
-        .style("stroke", "black");
+  var line = d3.line()
+    .x(function(d, i) { return x(xValues[i]); })
+    .y(function(d, i) { return y(yValues[i]); });
 
-      // Add the x and y axes
-      svg.append("g")
-          .attr("transform", "translate(0," + height + ")")
-          .call(d3.axisBottom(x));
+  svg.append("path")
+    .datum(filteredData)
+    .attr("class", "line")
+    .attr("d", line)
+    .style("fill", "none")
+    .style("stroke", "black");
 
-      svg.append("g")
-          .call(d3.axisLeft(y));
+  // year format on x axis
+  var xAxis = d3.axisBottom(x)
+    .tickFormat(d3.format("d")); // Use "d" format specifier to remove comma separator
+
+
+  svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+
+  svg.append("g")
+      .attr("class", "y-axis")
+      .call(d3.axisLeft(y));
+
+  // define a function to get y values for the selected option
+  function getYValues(data, selectedOption) {
+    return data.map(obj => obj[selectedOption]);
   }
+
+  // add a change event listener to the dropdown
+  d3.select("#indicator_dropdown").on("change", function() {
+    // update the yValues variable
+    yValues = getYValues(filteredData, selectedOption.value);
+    console.log(yValues)
+
+    // update the y scale domain
+    y.domain(d3.extent(yValues));
+
+    // Update the y axis
+    svg.select(".y-axis")
+      .transition()
+      .duration(500)
+      .call(d3.axisLeft(y));
+
+    // update the line
+    svg.select(".line")
+      .datum(filteredData)
+      .transition()
+      .duration(1000)
+      .attr("d", line);
+  });
+}
